7347756c0b4aaca660a38c0434d42b5fce5fae7e
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2022-09-23 15:38:20 +0200

    datapath: allow conntrack in non-initial user namespace
    Upstream commit:
        commit 59cd7377660a76780bfdd9fd26da058bcca5320d
        Author: Michael Weiß <michael.weiss@aisec.fraunhofer.de>
        Date:   Fri Sep 23 15:38:20 2022 +0200
    
        net: openvswitch: allow conntrack in non-initial user namespace
    
        Similar to the previous commit, the Netlink interface of the OVS
        conntrack module was restricted to global CAP_NET_ADMIN by using
        GENL_ADMIN_PERM. This is changed to GENL_UNS_ADMIN_PERM to support
        unprivileged containers in non-initial user namespace.
    
        Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>
        Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    
    Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

--- datapath/conntrack.c
+++ datapath/conntrack.c
@@ -2036,7 +2036,8 @@ static int ovs_ct_limit_set_zone_limit(struct nlattr *nla_zone_limit,
 		} else {
 			struct ovs_ct_limit *ct_limit;
 
-			ct_limit = kmalloc(sizeof(*ct_limit), GFP_KERNEL);
+			ct_limit = kmalloc(sizeof(*ct_limit),
+					   GFP_KERNEL_ACCOUNT);
 			if (!ct_limit)
 				return -ENOMEM;
 
@@ -2312,8 +2313,9 @@ static struct genl_ops ct_limit_genl_ops[] = {
 #ifdef HAVE_GENL_VALIDATE_FLAGS
 		.validate = GENL_DONT_VALIDATE_STRICT | GENL_DONT_VALIDATE_DUMP,
 #endif
-		.flags = GENL_ADMIN_PERM, /* Requires CAP_NET_ADMIN
-					   * privilege. */
+		.flags = GENL_UNS_ADMIN_PERM, /* Requires CAP_NET_ADMIN
+					       * privilege.
+					       */
 #ifdef HAVE_GENL_OPS_POLICY
 		.policy = ct_limit_policy,
 #endif
@@ -2323,8 +2325,9 @@ static struct genl_ops ct_limit_genl_ops[] = {
 #ifdef HAVE_GENL_VALIDATE_FLAGS
 		.validate = GENL_DONT_VALIDATE_STRICT | GENL_DONT_VALIDATE_DUMP,
 #endif
-		.flags = GENL_ADMIN_PERM, /* Requires CAP_NET_ADMIN
-					   * privilege. */
+		.flags = GENL_UNS_ADMIN_PERM, /* Requires CAP_NET_ADMIN
+					       * privilege.
+					       */
 #ifdef HAVE_GENL_OPS_POLICY
 		.policy = ct_limit_policy,
 #endif
